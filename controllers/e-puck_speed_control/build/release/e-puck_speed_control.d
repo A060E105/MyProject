8bb47483fcc50c3008032af7f5b121bb2d47d1a9
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-04-13 09:57:48 +0800

    add e-puck_speed_control

---
+++ controllers/e-puck_speed_control/build/release/e-puck_speed_control.d
@@ -0,0 +1,5 @@
+build/release/e-puck_speed_control.o: e-puck_speed_control.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
---
+++ controllers/e-puck_speed_control/e-puck_speed_control.c
@@ -0,0 +1,44 @@
+/*
+ * File:          e-puck_speed_control.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/motor.h>, etc.
+ */
+#include <webots/robot.h>
+
+#include <webots/motor.h>
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+#define MAX_SPEED 6.28
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv) {
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+
+  WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
+  WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+
+  wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
+  wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
+
+  while (wb_robot_step(TIME_STEP) != -1);
+
+  wb_robot_cleanup();
+
+  return 0;
+}
